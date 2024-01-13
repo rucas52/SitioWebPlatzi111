52cdb0d9ceb21876c6ba1dae6c793763d912ace6
Ruth Castillo <rucas52@gmail.com>
2024-01-13 18:14:42 -0500

    4to Commit

--- obj/Debug/net7.0/PlatziWebAppMvc.AssemblyInfo.cs
+++ obj/Debug/net7.0/PlatziWebAppMvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatziWebAppMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0116a9e9dec2619fd46893376ed8475bf29dc8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d193f8d84ffb03017ee000b31c3dd7797db8303c")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatziWebAppMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatziWebAppMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
