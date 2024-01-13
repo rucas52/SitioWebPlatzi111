d8e0aa810a31d3e4c97a3f539b7e5d5d5dd574f7
Ruth Castillo <rucas52@gmail.com>
2024-01-13 17:49:25 -0500

    Proyecto integrado

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PlatziWebAppMvc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
